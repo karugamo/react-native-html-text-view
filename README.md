6fc3f179e453e9227cc5781fbfc49bfd3f575ff6
Finn Pauls <mbp@pauls.dev>
2024-05-09 14:06:49 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# react-native-html-text
+
+Natively render html into a seletable text
+
+## Installation
+
+```sh
+npm install react-native-html-text
+```
+
+## Usage
+
+```js
+import { HtmlTextView } from "react-native-html-text";
+
+// ...
+
+<HtmlTextView color="tomato" />
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import {
+  requireNativeComponent,
+  UIManager,
+  Platform,
+  type ViewStyle,
+} from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-html-text' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+type HtmlTextProps = {
+  color: string;
+  style: ViewStyle;
+};
+
+const ComponentName = 'HtmlTextView';
+
+export const HtmlTextView =
+  UIManager.getViewManagerConfig(ComponentName) != null
+    ? requireNativeComponent<HtmlTextProps>(ComponentName)
+    : () => {
+        throw new Error(LINKING_ERROR);
+      };
